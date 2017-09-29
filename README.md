34562e5f910ab3b3ff239723c3facf582c99de34
System <none>
2017-09-29 21:50:45 +0000

    😳🕶 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# timestamp_microservice
-fCC Timestamp Microservice
+Free Code Camp | Timestamp Microservice
+=========================
+
+This microservice returns the date specificed in the URL as both a unix timestamp and a natural language date. The return format is JSON. 
+In order for this service to work, you must append to the current URL the date you would like returned.
+
+Example
+-------------
+`https://eight-cowl.glitch.me/December%2015,%202015`
+`https://eight-cowl.glitch.me/1450137600`
+
+Returns
+-------------
+
+`{ "unix": 1450137600, "natural": "December 15, 2015" }`
+
+Developed by DeSean Ellis - Free Code Camp
---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var dateFormat = require('dateformat');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/:timestamp", function (req, res) {
+  
+  //Non-numeric regex check
+  var pattern = /[^0-9]/g;
+  var returnDate = {unix: null, natural: null};
+  
+  //Check if timestamp is Integer
+  var timestamp = req.params.timestamp;
+  if (pattern.test(timestamp)) {
+        //Is not a valid Integer
+        try {
+          returnDate.unix = new Date(dateFormat(timestamp, "longDate")).getTime();
+          returnDate.natural = dateFormat(timestamp, "longDate");
+        } catch(ex) {
+          console.log(ex);
+        }
+      } else {
+        //Is a valid Integer
+        returnDate.unix = Number(timestamp);
+        returnDate.natural = dateFormat(new Date(Number(timestamp)), "longDate");
+      }
+  
+  res.json(returnDate);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
